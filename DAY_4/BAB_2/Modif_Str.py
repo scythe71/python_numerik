c5837c1f94fc0eead0dfdf78ecc873f323b2647f
Ramma <ammarshiddiq2@gmail.com>
2025-09-26 23:10:36 +0700

    DAY_26/9

--- DAY_4/BAB_2/Modif_Str.py
+++ DAY_4/BAB_2/Modif_Str.py
@@ -1,8 +1,21 @@
 # Modifikasi String
-a = "Ammar"
+a = "Ammar Shiddiq"
 
 # Huruf Besar
-print(a.upper()) # Output -> AMMAR
+print(a.upper()) # Output -> AMMAR SHIDDIQ
 
 # Huruf kecil
-print(a.lower()) # Output -> ammar
+print(a.lower()) # Output -> ammar shiddiq
+
+# Menghapus Spasi Putih = adalah spasi pada awal dan akhir string
+print(a.strip()) # Output -> "Ammar Shiddiq" bukan " Ammar Shiddiq"
+
+# Memisahkan String
+b = "Hello, Ammar"
+print(b.split(",")) # Output -> ['Hello', ' Ammar']
+
+# Menggabungkan String = bisa menggunakan operator tambah (+)
+c = "Hello"
+d = "World"
+e = c + ", " + d # tambahkan spasi dan koma
+print(e)
---
+++ DAY_4/BAB_3/bool_value.py
@@ -0,0 +1,12 @@
+# Boolean terbagi menjadi dua nilai, yaitu True dan False
+print(10 > 9)  # True
+print(10 == 9)  # False
+print(10 < 9)  # False
+
+# Menggunakan pernyataan if untuk mengecek nilai boolean
+a = 200
+b = 33
+if b > a:
+    print("b lebih besar dari a")
+else:
+    print("b tidak lebih besar dari a")

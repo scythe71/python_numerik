7aad8a612b3c6a7acd05f334cb0ab8a8e53ea4ad
Ramma <ammarshiddiq2@gmail.com>
2025-09-28 21:23:22 +0700

    Jaga-Jaga

---
+++ DAY_4/BAB_4/list.py
@@ -0,0 +1,5 @@
+# List adalah salah satu dari 4 tipe data yang ada di Python untuk menampung sekumpulan data
+# List adalah tipe data yang paling fleksibel di Python
+
+List = [1, 2, 3, 4, 5]
+print(List) # Output: [1, 2, 3, 4, 5]
